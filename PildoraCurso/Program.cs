5d1519c1e969d961f91b5d6d6ae240874e452c0a
antonioaguilarmedina8@gmail.com <antonioaguilarmedina8@gmail.com>
2023-06-16 14:45:40 -0600

    Agregar archivos de proyecto.

---
+++ PildoraCurso/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace PildoraCurso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //   Console.WriteLine( sumaNumeros(7,9));
+         //   Console.WriteLine(divideNumeros(18, 7));
+
+            // Console.WriteLine("Hello World!");
+            #region --------video4---------
+            //int edad;
+          // edad = 28;
+            // Console.WriteLine(edad);
+            #endregion
+            #region --video5--
+            // int edad = 19;
+            // edad++;
+            // Console.WriteLine("tienes una edad de "+edad+"años");
+            //interpoblacion de strings
+            //Console.WriteLine($"tienes una edad{edad} años");
+            #endregion
+            #region --usodeconstantev6--
+            /*var edadPersona=27;
+            Console.WriteLine(edadPersona);*/
+            // double temperatura = 34.5;
+            //int temperaturacancun;
+            //conversion explicita o casting
+            //  temperaturacancun = (int) temperatura;
+            //conversion implicita
+            // int habitantesciudad = 10000;
+            //  long HabitantesCiudad2023 = habitantesciudad;
+            //  float pesomaterial = 5.75F;
+            //  double pesomaterialprec = pesomaterial;
+
+            //  Console.WriteLine("latemperatura es "+temperaturacancun);
+            //  Console.WriteLine("los Habitantes son"+HabitantesCiudad2023);
+            // Console.WriteLine("El peso de los materiales son"+pesomaterialprec);    
+            #endregion
+            #region ---video7 texto a numerico--
+            // Console.WriteLine("introduce un numero");
+            //int num1= int.Parse( Console.ReadLine());
+            //  Console.WriteLine("introduce un numero");
+            // int num2 = int.Parse(Console.ReadLine());
+            //  Console.WriteLine("introduce un numero"+(num1+num2));
+
+            #endregion
+            #region ---video8usodeconstante----
+            /*const int VALOR=5;
+            const int VALOR2 = 7;
+            Console.WriteLine(  "el valor de la constante es:{0}\n{1} ",VALOR,VALOR2);*/
+            ///radio de un circulo
+            // const double PI = 3.1416;
+            // Console.WriteLine("Introduce la medida del radio");
+            // double radio=double.Parse(Console.ReadLine());
+            //double area = radio * radio * PI;
+            // double area=Math.Pow(radio,2)*PI;
+            // Console.WriteLine($"El area del circulo es: { area}");
+
+
+
+            #endregion
+        }
+        #region -----v9-v10-v11Metodos----
+        // static void mensajeEnPantalla() {
+        //   Console.WriteLine("mensage desde el metodo mensageenpantalla "  );
+        /* static void sumaNumeros(int num1, int num2) { Console.WriteLine(  $"la suma de los numeroses:{num1+num2}"); 
+         }*/
+        //static int sumaNumeros(int num1, int num2) { return num1 + num2; }
+        //static double divideNumeros(double num1, int num2) { return num1 / num2; }
+
+
+        #endregion
+
+    }
+
+}
